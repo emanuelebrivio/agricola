20efbd4b0fbc5f1e4e4221bb6d10b98f6802f541
emanuelebrivio <emanuele.brivio.work@proton.me>
2025-03-14 15:07:21 +0100

    Remove redirects

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'app.agricolabrivio.com',
-          },
-        ],
-        destination: '/app/:path*',
-      },
-    ]
-  },
+
 };
 
 export default nextConfig;
