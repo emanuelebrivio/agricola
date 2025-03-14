2e38eb22f0bf6b6df94ae4d6d3983e8d4adc797e
emanuelebrivio <emanuele.brivio.work@proton.me>
2025-03-14 15:29:50 +0100

    Get subdomain with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getValidSubdomain } from '@/utils/subdomain';
+
+// RegExp for public files
+const PUBLIC_FILE = /\.(.*)$/; // Files
+
+export async function middleware(req: NextRequest) {
+  // Clone the URL
+  const url = req.nextUrl.clone();
+
+  // Skip public files
+  if (PUBLIC_FILE.test(url.pathname) || url.pathname.includes('_next')) return;
+
+  const host = req.headers.get('host');
+  const subdomain = getValidSubdomain(host);
+  if (subdomain === 'app') {
+    // Subdomain available, rewriting
+    url.pathname = `/${subdomain}${url.pathname}`;
+  }
+
+  return NextResponse.rewrite(url);
+}
