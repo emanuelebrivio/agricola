707bfbf1ea252bd7a0ad139b6efae4c232494af6
emanuelebrivio <emanuele.brivio.work@proton.me>
2025-03-14 15:02:09 +0100

    Another test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'app.agricolabrivio.com',
+          },
+        ],
+        destination: '/app/:path*',
+      },
+    ]
+  },
 };
-
-
 
 export default nextConfig;
